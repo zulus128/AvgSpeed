4491389ffcd40643bb0a7358fcd2cfcb98c065d8
Vadim Kassin <vgzulus@gmail.com>
2025-12-15 23:39:50 +0300

    Initial Commit

---
+++ AvgSpeed Watch App/AvgSpeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvgSpeedApp.swift
+//  AvgSpeed Watch App
+//
+//  Created by VADIM KASSIN on 15.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct AvgSpeed_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AvgSpeed Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AvgSpeed Watch App
+//
+//  Created by VADIM KASSIN on 15.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
